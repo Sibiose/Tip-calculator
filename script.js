25a84ae2a995b29a9b1d735e285eaa8d124fee14
Sibiose <corneliu_maris@yahoo.com>
2022-05-07 22:14:51 +0300

    Recent update

--- script.js
+++ script.js
@@ -4,128 +4,102 @@ let customInput = document.querySelector(".custom");
 let totalPerPerson = document.querySelector(".peopletotal");
 let totalTip = document.querySelector(".tiptotal");
 let error = document.querySelector(".error");
-let btnReset = document.querySelector(".reset")
+let btnReset = document.querySelector(".reset");
 
-let btn5 = document.querySelector(".percent5");
-let btn10 = document.querySelector(".percent10");
-let btn15 = document.querySelector(".percent15");
-let btn25 = document.querySelector(".percent25");
-let btn50 = document.querySelector(".percent50");
+const percentBtns = [...document.querySelectorAll(".percent-btn")];
+const values = [5, 10, 15, 25, 50];
 
-function calculateSplit(){
-	let splitSum = billInput.value / peopleInput.value;
-	return parseFloat(splitSum).toFixed(2);
+percentBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    updateTip(values[percentBtns.indexOf(btn)] / 100);
+  });
+});
 
-};
+/**
+ * A function that calculates the split sum between the number of people that are paying
+ */
+function calculateSplit() {
+  let splitSum = billInput.value / peopleInput.value;
+  return parseFloat(splitSum).toFixed(2);
+}
 
-function calculateTip(percent){
-	let splitTip = billInput.value * percent / peopleInput.value;
-	return parseFloat(splitTip).toFixed(2);
-};
- 
+/**
+ * A function that calculates the tip each person should pay depending on the percent chosen
+ */
+function calculateTip(percent) {
+  let splitTip = (billInput.value * percent) / peopleInput.value;
+  return parseFloat(splitTip).toFixed(2);
+}
+
 /*Functions to update each tip amount*/
 
-
-
-function updateTotalPerPerson(){
-	totalPerPerson.innerHTML = "$ " + calculateSplit();
+function updateTotalPerPerson() {
+  totalPerPerson.innerText = `$ ${calculateSplit()} `;
 }
-;
 
-function UpdateTip5(){
- 	checkForErrors();
- 	updateTotalPerPerson();
-	totalTip.innerHTML = "$ " + calculateTip(0.05);
-
+const updateTip = (percent) => {
+  if (handleError()) {
+    updateTotalPerPerson();
+    totalTip.innerText = `$ ${calculateTip(percent)}`;
+  }
 };
 
-function UpdateTip10(){
-	checkForErrors();
-	updateTotalPerPerson();
-	totalTip.innerHTML = "$ " + calculateTip(0.10);
-	}
-;
-
-function UpdateTip15(){
-	checkForErrors();
-	updateTotalPerPerson();
-	totalTip.innerHTML = "$ " + calculateTip(0.15);
-	}
-;
-
-function UpdateTip25(){
-	checkForErrors();
-	updateTotalPerPerson();
-	totalTip.innerHTML = "$ " + calculateTip(0.25);
-	}
-;
-
-function UpdateTip50(){
-	checkForErrors();
-	updateTotalPerPerson();
-	totalTip.innerHTML = "$ " + calculateTip(0.5);
-	}
-;
-
-function UpdateTipCustom(){
-	checkForErrors();
-		updateTotalPerPerson();
-		totalTip.innerHTML = "$ " + calculateTip(customInput.value / 100);
+function UpdateTipCustom() {
+  updateTip(customInput.value / 100);
 }
 
-function keypressUpdateTotal(event){
-	if(peopleInput.value !== "" && peopleInput.value != 0 && event.keyCode === 13){
-	updateTotalPerPerson()
-}
+// Validation of input and error handling
+/**
+ * A function that validates input and resets the app if input is not valid
+ */
+function handleError() {
+  if (
+    peopleInput.value <= 0 ||
+    peopleInput.value === undefined ||
+    billInput.value <= 0 ||
+    billInput.value === undefined
+  ) {
+    reset();
+    showError();
+    return false;
+  } else {
+    hideError();
+    return true;
+  }
 }
 
-function keypressUpdateTipCustom(event){
-if(customInput.value !== "" && event.keyCode === 13){
-	checkForErrors();
-	UpdateTipCustom();
+function showError() {
+  error.classList.remove("hide");
 }
+function hideError() {
+  error.classList.add("hide");
 }
 
+//
 
-btn5.addEventListener("click", UpdateTip5);
-btn10.addEventListener("click", UpdateTip10);
-btn15.addEventListener("click", UpdateTip15);
-btn25.addEventListener("click", UpdateTip25);
-btn50.addEventListener("click", UpdateTip50);
+function keypressUpdateTotal(event) {
+  if (event.keyCode === 13 && handleError()) {
+    updateTotalPerPerson();
+  }
+}
+
+function keypressUpdateTipCustom(event) {
+  if (event.keyCode === 13 && handleError()) {
+    UpdateTipCustom();
+  }
+}
+
 customInput.addEventListener("keypress", keypressUpdateTipCustom);
 peopleInput.addEventListener("keypress", keypressUpdateTotal);
-
-/*ERRORS*/
-
-function removeError(){
-		error.classList.remove("hide");
-	
-	}
-function addError(){
-	error.classList.add("hide");
-}
-
-
+btnReset.addEventListener("click", reset);
 
 /*RESET FUNCTION*/
 
-function reset(){
-	billInput.value = "";
-	peopleInput.value = "";
-	totalTip.innerHTML = "$ 0.00";
-	totalPerPerson.innerHTML = "$ 0.00";
-	customInput.value = "";
-	addError();
+function reset() {
+  billInput.value = "";
+  peopleInput.value = "";
+  totalTip.innerText = "$ 0.00";
+  totalPerPerson.innerText = "$ 0.00";
+  customInput.value = "";
+  hideError();
 }
-
-btnReset.addEventListener("click", reset);
-
-function checkForErrors(){
- 	if(peopleInput.value == 0 || peopleInput.value === ""){
- 		reset();
- 		removeError();
- 
- 	} else if(peopleInput.value != 0 || peopleInput.value ===""){
- 	addError()
- 	}
- }
